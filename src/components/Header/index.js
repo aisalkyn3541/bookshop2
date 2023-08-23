883c9111810489cb40a6ef3978911f9e9168394b
Aisalkyn <aisalkyn3541@gmail.com>
2023-08-23 18:23:47 +0600

    dhfg

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import logo from '../../image/Logo (1).png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div id='header' >
+      <div className='container'>
+        <div className='header'>
+          <div header-nav>
+            <img src={logo} alt="" />
+
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
